ab545e26185cf3432c121ac63bebc67616563eaa
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2019-04-21 14:38:34 +0300

    includes MyCurrenciesContainer

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import React from 'react';
+
 import Header from "./header/Header";
-import MyCurrencies from "./content/myCurrencies/MyCurrencies"
+
 import "./App.css"
 import {Route, Router} from "react-router-dom";
+
 import HeaderStart from "./headerStart/HeaderStart";
 import LogIn from "./content/logIn/LogIn";
 
 import SignPage from "./signPage/SignPage"
 import SignUp from "./content/signUp/SignUp";
+
 import AllCurrenciesContainer from "./content/allCurrencies/AllCurrenciesContainer";
+import MyCurrenciesContainer from "./content/myCurrencies/MyCurrenciesContainer";
+
 import syncHistoryWithStore from "react-router-redux/src/sync";
 import {browserHistory, store} from "./redux/reduxStore";
 
 const history = syncHistoryWithStore(browserHistory, store);
 
 const App = (props) => {
-        return (
-            <Router history={history}>
-                <div className="App-layout">
-                    <div>
-                        <Route exact path = "/" render = {() => <HeaderStart/>}/>
-                        <Route path = "/myCurrencies" render = {() => <Header/>}/>
-                        <Route path = "/allCurrencies" render = {() => <Header/>}/>
-                        <Route path = "/SignUp" render = {() => <SignPage/>}/>
-                    </div>
-                    <div>
-                        <Route path = "/allCurrencies" render = {() => <AllCurrenciesContainer store={props.store}/>}/>
-                        <Route path = "/myCurrencies" render = {() => <MyCurrencies/>}/>
-                        <Route exact path = "/" render = {() => <LogIn/>}/>
-                        <Route path = "/SignUp" render = {() => <SignUp/>}/>
-                    </div>
+    return (
+        <Router history={history}>
+            <div className="App-layout">
+                <div>
+                    <Route exact path = "/" render = {() => <HeaderStart/>}/>
+                    <Route path = "/myCurrencies" render = {() => <Header/>}/>
+                    <Route path = "/allCurrencies" render = {() => <Header/>}/>
+                    <Route path = "/SignUp" render = {() => <SignPage/>}/>
                 </div>
-            </Router>
-        );
+                <div>
+                    <Route path = "/allCurrencies" render = {() => <AllCurrenciesContainer store={props.store}/>}/>
+                    <Route path = "/myCurrencies" render = {() => <MyCurrenciesContainer store={props.store}/>}/>
+                    <Route exact path = "/" render = {() => <LogIn/>}/>
+                    <Route path = "/SignUp" render = {() => <SignUp/>}/>
+                </div>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
