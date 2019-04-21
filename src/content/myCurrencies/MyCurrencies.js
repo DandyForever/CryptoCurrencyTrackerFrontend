ed22f399304373eaed2d65b83d39f311586c333c
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2019-04-21 14:39:41 +0300

    shows uploaded subscriptions from localhost

--- src/content/myCurrencies/MyCurrencies.js
+++ src/content/myCurrencies/MyCurrencies.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React from 'react';
+import Subscription from './items/Subscription'
 import style from './MyCurrencies.module.css'
 
+import {logger} from "redux-logger";
+
+
 const MyCurrencies = (props) => {
-  return (
-      <div className={style.myCurrencies}>
-          <div>
-              My first Currency
+
+    if (props.mustFetch){
+        props.fetchSubscriptions();
+        props.setMustFetch(false);
+    }
+
+    let subscriptions = props.subscriptions.map(subscription => <Subscription
+        key = {subscription.id}
+        id = {subscription.id}
+        subscriptionPK = {subscription.subscriptionPK}
+        currencyDto = {subscription.currencyDto}
+        userVal = {subscription.userVal}
+        totalAssets = {subscription.totalAssets}
+        deleteSubscription = {subscription.deleteSubscription}
+        />);
+
+    return (
+          <div className={style.myCurrencies}>
+              <table className={style.currenciesTable}>
+                  <thead>
+                  <tr>
+                      <th>Crypto Name</th>
+                      <th>Crypto Value</th>
+                      <th>Cash in USD</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {subscriptions}
+                  </tbody>
+              </table>
           </div>
-          <div>
-              My Second Currency
-          </div>
-          <div>
-              My Third Currency
-          </div>
-          <div>
-              My Fourth Currency
-          </div>
-          <div>
-              My Fifth Currency
-          </div>
-      </div>
   )
 };
 
